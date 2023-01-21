7ea487cde8717a7a00df1dd6738ab66573570d5e
JeanSuw <119563534+JeanSuw@users.noreply.github.com>
2023-01-20 21:12:11 -0800

    Added constant variables in the constant list

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+
+
+// Quiz Questions (All values are fixed)
+const problems = [
+    {
+        question: "",
+        choices: ["", "", "", ""],
+        answer: ""
+    },
+    {
+        question: "",
+        choices: ["", "", "", ""],
+        answer: ""
+    },
+    {
+        question: "",
+        choices: ["", "", "", ""],
+        answer: ""
+    }
+];
