f8ea6b2abe319bc10331cc86503e37bd7a54208a
JeanSuw <119563534+JeanSuw@users.noreply.github.com>
2023-01-23 02:53:09 -0800

    Added check for correct answer functions

--- js/scripts.js
+++ js/scripts.js
@@ -22,13 +22,16 @@ var correctCounts = 0, problemIndex = 0;
 
 // Reference Elements
 
-var maxTime, timeLeft, titleCardDiv, choiceA, choiceB, choiceC, choiceD, currentAnswer;
+var maxTime, timeLeft, startDiv, questionDiv, currentQuestion, choiceA, choiceB, choiceC, choiceD, currentAnswer;
 
 maxTime = 90;
 timeLeft = document.getElementById("timeRemaining");
 
 startDiv = document.getElementById("startingPage");
 questionDiv = document.getElementById("problems");
+currentQuestion = document.getElementById("currentQuestion");
+currentAnswer = document.getElementById("answerCheck");
+
 
 choiceA = document.getElementById("buttonA");
 choiceB = document.getElementById("buttonB");
@@ -37,7 +40,6 @@ choiceD = document.getElementById("buttonD");
 
 // Start Quiz function and time the user
 function startQuiz(){
-    
     timeLeft.textContent = maxTime; // Set time
 
     // Close title card when user press start by disable starting page (Title page)
@@ -50,18 +52,63 @@ function startQuiz(){
         timeLeft.textContent = maxTime;
         if (maxTime <= 0){
             clearInterval(startTimer);
-            if (problemIndex < problems.length -1){
+            if (problemIndex < problems.length-1){
                 // End the game
             }
         }
     }, 1000);
 
-
+    // Present with the next question
 };
 
-// Show the current question and choices on the screen
+// Show the next question and choices on the screen
+function nextProblem(){
+    currentQuestion.textContent = problems[problemIndex].question;
+    choiceA.textContent = problems[problemIndex].choices[0];
+    choiceB.textContent = problems[problemIndex].choices[1];
+    choiceC.textContent = problems[problemIndex].choices[2];
+    choiceD.textContent = problems[problemIndex].choices[3];
+}
 
 // Check answers by showing if the user choose correct or incorrect answers
+function isCorrect(){
+    currentAnswer.style.display = "block";
+
+    if (problems[problemIndex].answer === problems[problemIndex].choices[answer]){
+        // isCorrect, then add 5 points
+        currentAnswer.textContent = "Your answer (" + problems[problemIndex].choices[answer] + ") is Correct!";
+    }else{
+        // if not isCorrect, then subtract by 5 on the timer
+        maxTime -= 5;
+        timeLeft.textContent = maxTime;
+        currentAnswer.textContent = "Wrong! Your time is deduct \n" + "The correct answer is " + problems[problemIndex].answer;
+    }
+    
+    // increment index for the next question
+    problemIndex++; 
+    if (problemIndex < problems.length){
+        nextProblem();
+    }else{
+        // end the game
+    }
+
+}
+
+function clickA(){
+    isCorrect(0);
+}
+
+function clickB(){
+    isCorrect(1);
+}
+
+function clickC(){
+    isCorrect(2);
+}
+
+function clickD(){
+    isCorrect(3);
+}
 
 
 
